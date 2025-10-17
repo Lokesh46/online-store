e18dc09a795aa18aad4a777e67d49f479a2bce57
lokesh immadi <lokeshimmadi22@gmail.com>
2025-10-17 17:37:40 +0530

    no more changes 2

--- backend/src/main/java/com/ecommerce/backend/config/CorsConfig.java
+++ backend/src/main/java/com/ecommerce/backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://ecommerce-frontend-er9y.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
